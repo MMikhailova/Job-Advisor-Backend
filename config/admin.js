cdd0c20ac4c12ed66ba94bca3d949221573a11e1
Maria <naplesaries2014@gmail.com>
2022-11-28 09:40:44 +0100

    changed admin

--- config/admin.js
+++ config/admin.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
     secret: env("ADMIN_JWT_SECRET", "e4350524b0689fc3f46e2407dae950e2"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT') || 'efzzjenvzjevjonesvjlnesjkn',
+    salt: env("API_TOKEN_SALT", "reallylodfdfdfdfdfdfdfdfdfdfngstring"),
   },
 });
